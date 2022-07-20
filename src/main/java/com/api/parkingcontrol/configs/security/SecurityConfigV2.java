e117d58edb3aecd589ea7a706ec2398cadbbafd1
GabrielCPDev <gabrielcpcontato@outlook.com>
2022-07-20 14:38:04 -0300

    implementando nova forma de configurar seguranca  usando spring security

--- src/main/java/com/api/parkingcontrol/configs/security/SecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrol/configs/security/SecurityConfigV2.java
@@ -9,9 +9,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class SecurityConfigV2 {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
     @Bean
@@ -19,22 +20,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return  new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET).permitAll()
-                .antMatchers(HttpMethod.POST, "/parking-spot/**").hasAnyRole("USER")
-                .antMatchers(HttpMethod.DELETE,"/roles/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/parking-spot/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/parking-spot/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE,"/parking-spot/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+                .and().csrf().disable();
+        return http.build();
     }
 }
